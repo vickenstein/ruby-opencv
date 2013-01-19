5b67b19d2b108f9da982e72fc07d601b615583d7
ser1zw <azariahsawtikes@gmail.com>
2013-01-20 03:56:06 +0900

    update tests of CvMat#optical_flow_*

--- test/test_cvmat_imageprocessing.rb
+++ test/test_cvmat_imageprocessing.rb
@@ -1798,30 +1798,30 @@ class TestCvMat_imageprocessing < OpenCVTestCase
 
     [curr.optical_flow_hs(prev, nil, nil, :lambda => 0.0005, :criteria => CvTermCriteria.new(1, 0.001)),
      curr.optical_flow_hs(prev)].each { |velx, vely|
-      assert_equal('d437cd896365c509b5d16fd5f2d7e498', hash_img(velx))
-      assert_equal('36ccbcafcd58b4d07dab058fb60eede6', hash_img(vely))
+      assert_in_delta(60, count_threshold(velx, 1), 20)
+      assert_in_delta(50, count_threshold(vely, 1), 20)
     }
 
     velx, vely = curr.optical_flow_hs(prev, nil, nil, :lambda => 0.001)
-    assert_equal('6a0133bcfa5057ef3d607429753d1f27', hash_img(velx))
-    assert_equal('dffbc0e267b08f3ca0098f27ecf61f6e', hash_img(vely))
+    assert_in_delta(60, count_threshold(velx, 1), 20)
+    assert_in_delta(50, count_threshold(vely, 1), 20)
 
     velx, vely = curr.optical_flow_hs(prev, nil, nil, :criteria => CvTermCriteria.new(10, 0.01))
-    assert_equal('5dc2bb2aaee70383da8b12c99cbf388b', hash_img(velx))
-    assert_equal('b53cec7a363ba7491cde61540813b827', hash_img(vely))
+    assert_in_delta(130, count_threshold(velx, 1), 20)
+    assert_in_delta(110, count_threshold(vely, 1), 20)
 
     prev_velx, prev_vely = curr.optical_flow_hs(prev)
     velx, vely = curr.optical_flow_hs(prev, prev_velx, prev_vely)
-    assert_equal('08b50f3d4cb4cbbe443fada293e6af02', hash_img(velx))
-    assert_equal('4d302c8267388995ec85a4a26da5ffcc', hash_img(vely))
+    assert_in_delta(70, count_threshold(velx, 1), 20)
+    assert_in_delta(60, count_threshold(vely, 1), 20)
 
     velx, vely = curr.optical_flow_hs(prev, prev_velx, prev_vely, :lambda => 0.001)
-    assert_equal('8bb08ee394719a70e24bfb8b428662cd', hash_img(velx))
-    assert_equal('f6c09e73160f0792e4527cdeea0e5573', hash_img(vely))
+    assert_in_delta(80, count_threshold(velx, 1), 20)
+    assert_in_delta(70, count_threshold(vely, 1), 20)
 
     velx, vely = curr.optical_flow_hs(prev, prev_velx, prev_vely, :criteria => CvTermCriteria.new(10, 0.01))
-    assert_equal('c32a8483e3aec3cd6c33bceeefb8d2f2', hash_img(velx))
-    assert_equal('da33e266aece70ed69dcf074acd8fd4e', hash_img(vely))
+    assert_in_delta(150, count_threshold(velx, 1), 20)
+    assert_in_delta(130, count_threshold(vely, 1), 20)
     
     assert_raise(TypeError) {
       curr.optical_flow_hs(DUMMY_OBJ)
@@ -1858,12 +1858,12 @@ class TestCvMat_imageprocessing < OpenCVTestCase
     }
     
     velx, vely = curr.optical_flow_lk(prev, CvSize.new(3, 3))
-    assert_equal('13333362f0daf6ad732006bd2a32e177', hash_img(velx))
-    assert_equal('45dc42034ab606dd61e34a5adc6a1c1b', hash_img(vely))
+    assert_in_delta(100, count_threshold(velx, 1), 20)
+    assert_in_delta(90, count_threshold(vely, 1), 20)
 
     velx, vely = curr.optical_flow_lk(prev, CvSize.new(5, 5))
-    assert_equal('d83c57805f9c074d0ad33a7522a75952', hash_img(velx))
-    assert_equal('df8fb2010b00d89090e7d9653781a68d', hash_img(vely))
+    assert_in_delta(180, count_threshold(velx, 1), 20)
+    assert_in_delta(150, count_threshold(vely, 1), 20)
 
     assert_raise(TypeError) {
       curr.optical_flow_lk(DUMMY_OBJ, CvSize.new(3, 3))
@@ -1896,26 +1896,26 @@ class TestCvMat_imageprocessing < OpenCVTestCase
     [curr.optical_flow_bm(prev, nil, nil, :block_size => CvSize.new(4, 4),
                           :shift_size => CvSize.new(1, 1), :max_range => CvSize.new(4, 4)),
      curr.optical_flow_bm(prev)].each { |velx, vely|
-      assert_equal('08e73a6fa9af7684a5eddc4f30fd46e7', hash_img(velx))
-      assert_equal('aabaf1b7393b950c2297f567b6f57d5d', hash_img(vely))
+      assert_in_delta(350, count_threshold(velx, 1), 30)
+      assert_in_delta(250, count_threshold(vely, 1), 30)
     }
-    
+
     velx, vely = curr.optical_flow_bm(prev, nil, nil, :block_size => CvSize.new(3, 3))
-    assert_equal('fe540dc1f0aec2d70b774286eafa3602', hash_img(velx))
-    assert_equal('c7cc0f055fe4708396ba6046c0f1c6b5', hash_img(vely))
+    assert_in_delta(280, count_threshold(velx, 1), 30)
+    assert_in_delta(200, count_threshold(vely, 1), 30)
 
     velx, vely = curr.optical_flow_bm(prev, nil, nil, :shift_size => CvSize.new(2, 2))
-    assert_equal('00ad7854afb6eb4e46f0fb31c312c5eb', hash_img(velx))
-    assert_equal('33215ec0bfa7f6b1424d6fadb2a48e0f', hash_img(vely))
+    assert_in_delta(80, count_threshold(velx, 1), 30)
+    assert_in_delta(60, count_threshold(vely, 1), 30)
 
     velx, vely = curr.optical_flow_bm(prev, nil, nil, :max_range => CvSize.new(5, 5))
-    assert_equal('ac10837eeee45fd80a24695cfaf9cfc7', hash_img(velx))
-    assert_equal('8c7011c26ac53eaf1fae1aa9324e5979', hash_img(vely))
+    assert_in_delta(400, count_threshold(velx, 1), 30)
+    assert_in_delta(300, count_threshold(vely, 1), 30)
     
     prev_velx, prev_vely = curr.optical_flow_bm(prev)
     velx, vely = curr.optical_flow_bm(prev, prev_velx, prev_vely)
-    assert_equal('6ad6b7a5c935379c0df4b9ec5666f3de', hash_img(velx))
-    assert_equal('b317b0b9d4fdb0e5cd40beb0dd4143b4', hash_img(vely))
+    assert_in_delta(350, count_threshold(velx, 1), 30)
+    assert_in_delta(270, count_threshold(vely, 1), 30)
 
     assert_raise(TypeError) {
       curr.optical_flow_bm(DUMMY_OBJ)
