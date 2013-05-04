ba32cc36ba727c971ffc130b898646bd2d747fce
ser1zw <azariahsawtikes@gmail.com>
2013-05-05 00:03:37 +0900

    change library file path to load

--- lib/opencv.rb
+++ lib/opencv.rb
@@ -2,9 +2,8 @@ require (File.dirname(__FILE__) + '/opencv/version')
 
 if RUBY_PLATFORM =~ /mingw|mswin/
   major, minor, subminor = RUBY_VERSION.split('.')
-  version_dir = (major.to_i >= 2) ? '2.0' : '1.9'
   begin
-    require "#{version_dir}/opencv.so"
+    require "#{major}.#{minor}/opencv.so"
   rescue LoadError
     require 'opencv.so'
   end
